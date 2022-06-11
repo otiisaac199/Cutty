e6b3212e13aa5c865694f00b22d19de256b800b5
Isaac <otichinazaisaac19@gmail.com>
2022-06-11 23:14:30 +0100

    still no media query

--- js/app.js
+++ js/app.js
@@ -2,6 +2,42 @@ const bars = document.querySelector(".bar");
 const menuUl = document.querySelector(".menu-ul");
 
 bars.addEventListener("click", () => {
-  // bars.classList.toggle(".active");
-  console.log("sorking");
+  bars.classList.toggle("active");
+  menuUl.classList.toggle("active-2");
+  console.log("working");
 });
+
+let items = document.querySelectorAll(".observer-p");
+
+let Observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((e) => {
+      e.target.classList.toggle("show", e.isIntersecting);
+      if (e.isIntersecting) {
+        Observer.unobserve(e.target);
+      }
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+items.forEach((i) => {
+  Observer.observe(i);
+});
+
+let sliderIndex = 0;
+showSlides();
+function showSlides() {
+  let slides = document.querySelectorAll(".slider");
+  let i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  sliderIndex++;
+  if (sliderIndex > slides.length) {
+    sliderIndex = 1;
+  }
+  slides[sliderIndex - 1].style.display = "block";
+  setTimeout(showSlides, 5000); //Changes elety 5 seconds
+}
