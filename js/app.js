d67e466f1c37a44ebbe688410997fb49dfa32672
Isaac <otichinazaisaac19@gmail.com>
2022-06-11 20:15:57 +0100

    no media query yet

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const bars = document.querySelector(".bar");
+const menuUl = document.querySelector(".menu-ul");
+
+bars.addEventListener("click", () => {
+  // bars.classList.toggle(".active");
+  console.log("sorking");
+});
